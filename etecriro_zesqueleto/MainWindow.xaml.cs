21edacea35d383ec500f2e95ad001f52cf2d4873
jslucasf <jslucasf17@gmail.com>
2019-07-07 10:07:40 -0300

    Adicionar arquivos de projeto.

---
+++ etecriro_zesqueleto/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿
+
+
+
+
+
+
+
+
+/* protonwarecsgo.ga | tiago.blog.br |  | | 
+#############                       #############                   #############                   #############
+#############                       #############                   #############                   #############
+###                                     ###                         ###                           ####   
+###                                     ###                         ###                           ####
+#########                               ###                         #########                    ####
+#########                               ###                         #########                    ####
+###                                     ###                         ###                          ####
+###                                     ###                         ###                           ####
+#############                           ###                         #############                  ############# 
+#############                           ###                         #############                  #############
+    */
+
+
+
+
+
+
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+namespace zsqueletobro
+{
+
+    public partial class MainWindow : Window
+    {
+
+        KinectSensor etecnect;
+
+        byte[] info_cores_sensor_kinect = null;
+        WriteableBitmap bmp_rgb_cores = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count == 0)
+            {
+                MessageBox.Show("CADE O USUÁRIO?");
+
+            }
+
+            etecnect = KinectSensor.KinectSensors.FirstOrDefault();
+
+            try
+            {        
+                etecnect.SkeletonStream.Enable();
+                etecnect.ColorStream.Enable();
+                etecnect.Start();
+            }
+            catch
+            {
+                Application.Current.Shutdown();
+            }
+                etecnect.SkeletonFrameReady += etecnect_SkeletonFrameReady;
+                etecnect.ColorFrameReady += etecnect_ColorFrameReady;
+        }
+
+        void etecnect_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame visualizacao = e.OpenColorImageFrame())
+            {
+                if (visualizacao == null) return;
+
+                if (info_cores_sensor_kinect == null)
+                    info_cores_sensor_kinect = new byte[visualizacao.PixelDataLength];
+
+                visualizacao.CopyPixelDataTo(info_cores_sensor_kinect);
+
+                if (bmp_rgb_cores == null)
+                {
+                    this.bmp_rgb_cores = new WriteableBitmap(
+                        visualizacao.Width,
+                        visualizacao.Height,
+                        96,
+                        96,
+                        PixelFormats.Bgr32,
+                        null);
+                }
+
+                this.bmp_rgb_cores.WritePixels(new Int32Rect(0, 0, visualizacao.Width, visualizacao.Height), info_cores_sensor_kinect, visualizacao.Width * visualizacao.BytesPerPixel, 0);
+                esqueletobugado.Background = new ImageBrush(bmp_rgb_cores);
+            }
+        }
+
+        void etecnect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            esqueletobugado.Children.Clear();
+            Skeleton[] esqueletos = null;
+
+            using (SkeletonFrame esqueletos_quadros_ms = e.OpenSkeletonFrame())
+            {
+                if (esqueletos_quadros_ms != null)
+                {
+                    esqueletos = new Skeleton[esqueletos_quadros_ms.SkeletonArrayLength];
+                    esqueletos_quadros_ms.CopySkeletonDataTo(esqueletos);
+                }
+            }
+
+            if (esqueletos == null) return;
+
+            foreach (Skeleton esqueleto in esqueletos)
+            {
+                if (esqueleto.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    //Oh, I'll never kill myself to save my soul
+                    Joint handJoint = esqueleto.Joints[JointType.HandRight];
+                    Joint elbowJoint = esqueleto.Joints[JointType.ElbowRight];            
+                    bones_esqueleto(esqueleto.Joints[JointType.Head], esqueleto.Joints[JointType.ShoulderCenter]);
+                    bones_esqueleto(esqueleto.Joints[JointType.ShoulderCenter], esqueleto.Joints[JointType.Spine]);
+                    bones_esqueleto(esqueleto.Joints[JointType.Spine], esqueleto.Joints[JointType.HipCenter]);
+                    bones_esqueleto(esqueleto.Joints[JointType.HipCenter], esqueleto.Joints[JointType.HipLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.HipLeft], esqueleto.Joints[JointType.KneeLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.KneeLeft], esqueleto.Joints[JointType.AnkleLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.AnkleLeft], esqueleto.Joints[JointType.FootLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.HipCenter], esqueleto.Joints[JointType.HipRight]);
+                    bones_esqueleto(esqueleto.Joints[JointType.HipRight], esqueleto.Joints[JointType.KneeRight]);
+                    bones_esqueleto(esqueleto.Joints[JointType.KneeRight], esqueleto.Joints[JointType.AnkleRight]);
+                    bones_esqueleto(esqueleto.Joints[JointType.AnkleRight], esqueleto.Joints[JointType.FootRight]);
+                    bones_esqueleto(esqueleto.Joints[JointType.ShoulderCenter], esqueleto.Joints[JointType.ShoulderLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.ShoulderLeft], esqueleto.Joints[JointType.ElbowLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.ElbowLeft], esqueleto.Joints[JointType.WristLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.WristLeft], esqueleto.Joints[JointType.HandLeft]);
+                    bones_esqueleto(esqueleto.Joints[JointType.ShoulderCenter], esqueleto.Joints[JointType.ShoulderRight]);
+                    bones_esqueleto(esqueleto.Joints[JointType.ShoulderRight], esqueleto.Joints[JointType.ElbowRight]);
+                    bones_esqueleto(esqueleto.Joints[JointType.ElbowRight], esqueleto.Joints[JointType.WristRight]);
+                    bones_esqueleto(esqueleto.Joints[JointType.WristRight], esqueleto.Joints[JointType.HandRight]);
+                    //I was gone, but how was I to know?
+                }
+            }
+        }
+
+        void bones_esqueleto(Joint j1, Joint j2)
+        {
+            Line esp_bones_esqueleto = new Line();
+            esp_bones_esqueleto.Stroke = new SolidColorBrush(Colors.Purple);
+            esp_bones_esqueleto.StrokeThickness = 5;
+            ColorImagePoint j1P = etecnect.CoordinateMapper.MapSkeletonPointToColorPoint(j1.Position, ColorImageFormat.RgbResolution640x480Fps30);
+            esp_bones_esqueleto.X1 = j1P.X;
+            esp_bones_esqueleto.Y1 = j1P.Y;
+            ColorImagePoint j2P = etecnect.CoordinateMapper.MapSkeletonPointToColorPoint(j2.Position, ColorImageFormat.RgbResolution640x480Fps30);
+            esp_bones_esqueleto.X2 = j2P.X;
+            esp_bones_esqueleto.Y2 = j2P.Y;
+            esqueletobugado.Children.Add(esp_bones_esqueleto);
+        }
+    }
+}
+
